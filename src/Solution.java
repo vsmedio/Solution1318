531b865a25f8a9167eebb447ed1d1f9f583e7472
Arvis Abromovics <info@vsmedio.lv>
2018-01-10 20:28:49 +0100

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+/* 
+Чтение файла
+Чтение файла
+1. Считать с консоли имя файла.
+2. Вывести в консоль(на экран) содержимое файла.
+3. Не забыть освободить ресурсы. Закрыть поток чтения с файла и поток ввода с клавиатуры.
+
+
+Требования:
+1. Программа должна считывать c консоли имя файла.
+2. Программа должна выводить на экран содержимое файла.
+3. Поток чтения из файла (FileInputStream) должен быть закрыт.
+4. BufferedReader также должен быть закрыт.
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        // напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String filename = reader.readLine();
+
+        BufferedReader file = new BufferedReader (new InputStreamReader(new FileInputStream(filename), "windows-1251"));
+
+        while (file.ready()) {
+            System.out.println(file.readLine());
+        }
+
+        file.close();
+        reader.close();
+
+    }
+}
